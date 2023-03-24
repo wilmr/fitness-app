7040e69aad4f9d9edd524382e57411522bc0bace
wilmer martinez <wilmermartinez@wilmers-Laptop.local>
2023-03-24 04:12:16 -0700

    more navbar component

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -1,9 +1,8 @@
-import NAvbar from "@/scenes/navbar";
+import Navbar from "@/scenes/navbar";
 
 function App() {
   return (
     <div className="app bg-gray-20">
-      app
       <Navbar />
     </div>
   );
--- gym-typescript/src/scenes/navbar/index.tsx
+++ gym-typescript/src/scenes/navbar/index.tsx
@@ -8,7 +8,27 @@ const Navbar = (props: Props) => {
   const flexBetween = "flex items-center justify-between";
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}></div>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/*LEFT SIDE*/}
+            <img alt="logo" src={Logo} />
+            {/* RIGHT SIDE */}
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <p>Home</p>
+                <p>Benefits</p>
+                <p>Our Classes</p>
+                <p>Contact Us</p>
+              </div>
+              <div className={`${flexBetween} gap=8`}>
+                <p>Sign in</p>
+                <button> Become a member</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
